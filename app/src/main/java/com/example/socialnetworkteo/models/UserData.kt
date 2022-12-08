8a837f2652d83d15013a96ce7ff487cb489923e9
Vladyslav Zoria <zorya.vladison@gmail.com>
2022-12-08 20:20:34 +0200

    Updated database and fix bugs

--- app/src/main/java/com/example/socialnetworkteo/models/UserData.kt
+++ app/src/main/java/com/example/socialnetworkteo/models/UserData.kt
@@ -15,95 +15,36 @@ class UserData {
     private var _userList = MutableLiveData<List<User>>().apply {
         value = listOf(
             User(
-                "Maks Rens",
+                "Ivan Ivanov",
                 "Admin",
-                35,
-                onlineStatus[(0..4).random()],
-                "maksRens@gmail.com",
-                R.drawable.user1,
-                "Cars",
-                "There should be a description of the character! There should be a description of the character!"
-            ),
-
-            User(
-                "Sara Derby",
-                "Admin",
-                23,
-                onlineStatus[(0..4).random()],
-                "saraDerby@gmail.com",
-                R.drawable.user2,
-                "Collecting cards",
-                "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!"
-            ),
-
-            User(
-                "Mark Twelve",
-                "User",
                 21,
                 onlineStatus[(0..4).random()],
-                "makrTwelve@gmail.com",
-                R.drawable.user3,
-                "Fishing",
-                "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!"
+                "ivan.ivanov@gmail.com",
+                R.drawable.avatar_6,
+                "Cars",
+                "Like fastest cars and drive!"
             ),
 
             User(
-                "Nick Tiger",
+                "Petr Petrov",
                 "User",
-                22,
+                44,
                 onlineStatus[(0..4).random()],
-                "nickTiger@gmail.com",
-                R.drawable.user4,
-                "Picking up",
-                "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!"
+                "petr.petrov@gmail.com",
+                R.drawable.avatar_1,
+                "Football",
+                "Like watching football on TV and drink beer!"
             ),
 
             User(
-                "Lora Brown",
+                "Maria Marva",
                 "User",
-                45,
+                18,
                 onlineStatus[(0..4).random()],
-                "loraDrown@gmail.com",
-                R.drawable.user5,
-                "Photograph",
-                "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!"
-            ),
-
-            User(
-                "Semi Worner",
-                "User",
-                29,
-                onlineStatus[(0..4).random()],
-                "semiWorner@gmail.com",
-                R.drawable.user6,
-                "Biking",
-                "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!"
-            ),
-
-            User(
-                "Susan Garbel",
-                "User",
-                30,
-                onlineStatus[(0..4).random()],
-                "susanGarbel@gmail.com",
-                R.drawable.user7,
-                "Rock climbing",
-                "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!" + "There should be a description of the character! There should be a description of the character!" +
-                        "There should be a description of the character! There should be a description of the character!"
+                "maria@gmail.com",
+                R.drawable.avatar_13,
+                "Cooking",
+                "Like cooking and eat tasty!"
             )
         )
     }
